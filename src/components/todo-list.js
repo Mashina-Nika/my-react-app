a9b554e9e9f32cc297c950c40899f4d3c2ae70f3
Mashina-Nika <mashina.veronika@list.ru>
2025-01-29 15:00:55 +0500

    8th step

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,13 +1,29 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
     return(
         <ul>
-            <li><TodoListItem label="Check children's works"/></li>
-            <li><TodoListItem label="Make new files for test"/></li>
-            <li><TodoListItem label="Go for a walk"/></li>
-            <li><TodoListItem label="Read 5 pages of current book" important/>/></li>
+            <li>
+                <TodoListItem
+                    label={todos[0].label}
+                    important={todos[0].important}/>
+            </li>
+            <li>
+                <TodoListItem
+                    label={todos[1].label}
+                    important={todos[1].important}/>
+            </li>
+            <li>
+                <TodoListItem
+                    label={todos[2].label}
+                    important={todos[2].important}/>
+            </li>
+            <li>
+                <TodoListItem
+                    label={todos[3].label}
+                    important={todos[3].important}/>
+            </li>
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+    const todoData = [
+        {label:'Check works', important:false},
+        {label: 'Make tests', important: true},
+        {label: 'Go for a walk', important: false},
+    ];
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
             <TodoList/>
+            <TodoList todos = {todoData}/>
         </div>
     );
 }
