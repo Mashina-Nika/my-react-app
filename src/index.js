d58a40ecdaabcda871a5710f012e0c45c9a2e956
Mashina-Nika <mashina.veronika@list.ru>
2025-02-04 12:54:45 +0500

    13th step

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
     const todoData = [
@@ -17,6 +18,7 @@ const App = () => {
             <AppHeader />
             <SearchPanel />
             <TodoList todos = {todoData}/>
+            <ItemStatusFilter/>
         </div>
     );
 }
