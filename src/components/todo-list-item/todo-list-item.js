daa07cefddcd6f59e2649b7b360ed6e28a6b14f8
Mashina-Nika <mashina.veronika@list.ru>
2025-02-05 13:35:42 +0500

    18th step

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
+export default class TodoListItem extends Component {
+    render() {
 
-    return (
-        <span className="todo-list-item">
+        const {label, important = false} = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+        return (
+            <span className="todo-list-item">
       <span
           className="todo-list-item-label"
           style={style}>
@@ -23,7 +25,6 @@ const TodoListItem = ({label, important = false}) => {
         <i className="fa fa-trash-o"/>
       </button>
     </span>
-    );
-};
-
-export default TodoListItem;
+        );
+    };
+}
