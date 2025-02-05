a983203fb35956a9383facf304e2562db3fcef5f
Mashina-Nika <mashina.veronika@list.ru>
2025-02-05 13:49:14 +0500

    20th step

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-    constructor() {
-        super();
-        this.onLabelClick = () => {
-            console.log(`Done: ${this.props.label}`);
-        };
-    }
+
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    };
 
     render() {
 
