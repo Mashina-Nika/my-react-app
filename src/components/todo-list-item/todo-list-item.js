710c11c93c5c5a8bcfbefc1e0f327e551edc4a5e
Mashina-Nika <mashina.veronika@list.ru>
2025-02-11 22:33:24 +0500

    21th step

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,29 +3,43 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false
+    };
+
     onLabelClick = () => {
-        console.log(`Done: ${this.props.label}`);
+        this.setState(
+            {done: true}
+        )
     };
 
     render() {
 
         const {label, important = false} = this.props;
+        const {done} = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
         return (
-            <span className="todo-list-item">
-      <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={ this.onLabelClick }>
+            <span className={classNames}>
+            <span
+                className="todo-list-item-label"
+                style={style}
+                onClick={this.onLabelClick}>
         {label}
       </span>
+
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
         <i className="fa fa-exclamation"/>
       </button>
+
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
         <i className="fa fa-trash-o"/>
